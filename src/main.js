da24faa12965a6dc3d8f2a7d6f5738494056693a
cjguaguatai <1071873238@qq.com>
2022-07-13 12:18:41 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'lib-flexible'
+// 引入全局css样式
+import '@/style/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
